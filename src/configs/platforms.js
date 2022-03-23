be04ae9453832ad3a483527ce6b7950f6abe1f52
bepyan <bepyan@naver.com>
2022-03-23 18:44:06 +0900

    feat. 천장 만들기

--- src/configs/platforms.js
+++ src/configs/platforms.js
@@ -3,6 +3,7 @@ export let platforms;
 export function configPlatforms() {
   platforms = this.physics.add.staticGroup();
   platforms.create(400, 568, "ground").setScale(2).refreshBody();
+  platforms.create(400, 0, "ground").setScale(2).refreshBody();
   platforms.create(600, 400, "ground");
   platforms.create(50, 250, "ground");
   platforms.create(750, 220, "ground");
--- src/models/game.js
+++ src/models/game.js
@@ -5,7 +5,7 @@ export let score = 0;
 export let scoreText;
 
 export function configScoreText() {
-  scoreText = this.add.text(16, 16, "score: 0", {
+  scoreText = this.add.text(16, 40, "Score: 0", {
     fontSize: "32px",
     fill: "#000",
   });
